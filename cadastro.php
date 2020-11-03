646f326ecb69e5d90e9d57e5347a06617335fe33
Giih97 <gbamdev9@gmail.com>
2020-11-03 17:14:09 -0300

    incluindo icone na aba do navegador

--- cadastro.php
+++ cadastro.php
@@ -10,6 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Mind Pill</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="icon" href="image/logo01.png">
         <style>
             body {
                 background: rgb(85, 221, 187);
--- trilhaIN.php
+++ trilhaIN.php
@@ -10,6 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- <link rel="stylesheet" type="text/css" href="./css/trilha.css"> -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="icon" href="image/logo01.png">
 
         <title>Mind Pill - Trilha IN</title>
         <style>
