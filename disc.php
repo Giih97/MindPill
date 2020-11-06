ac31b20c177a4cd24daa4ffd721a4dbd1280c82c
andrem91 <andrem166@gmail.com>
2020-11-05 21:09:52 -0300

    Correção ortográfica

--- disc.php
+++ disc.php
@@ -30,7 +30,7 @@
             ?>
         </header>
                 <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                <strong>PAGINA EM CONSTRUÇÃO!</strong> ,  !
+                <strong>PAGINA EM CONSTRUÇÃO!</strong>
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
